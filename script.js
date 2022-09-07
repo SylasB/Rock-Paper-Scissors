6a182223d37c271803402649c96e6523d7bd62f4
SylasB <Sylasboniek@gmail.com>
2022-09-07 13:17:56 -0600

    Changed the computerChoice javascript and added a way for the player to select their choice.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const choice = ["rock", "paper", "scissors"];
+//This is the section of code to let the computer find a random choice.
+const choices = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
-    const index = Math.floor(Math.random() * choice.length);
-    console.log(index, choice[index]);
+function playRound(round) {
+    const playerSelection = playerChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playerSelection, computerSelection);
 }
 
-getComputerChoice();
+function getComputerChoice() {
+    return [Math.floor(Math.random() * choices.length)];
+}
+
+function playerChoice() {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    
+}
